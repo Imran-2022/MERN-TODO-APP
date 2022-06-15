e5985b18ff143a94c52a0aa7d8f1094a64af88dc
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-06-16 05:47:32 +0600

    remove all completed tasks.

--- src/features/to-do/ViewTodo.js
+++ src/features/to-do/ViewTodo.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+const axios = require('axios').default;
 const ViewTodo = () => {
   const [todo, setTodo] = useState([])
   const [search, setSearchResult] = useState([])
@@ -10,7 +11,7 @@ const ViewTodo = () => {
         setTodo(data)
       })
   }, [])
-  const [id,setId] = useState("")
+  const [id, setId] = useState("")
 
   const [text, setText] = useState("");
 
@@ -20,41 +21,53 @@ const ViewTodo = () => {
     setSearchResult(searchResult)
   }, [text])
 
-useEffect(() => {
-  if(id){
-    const data= todo.filter(dt =>dt._id==id)
-    const url = `http://localhost:8080/api/todo/${id}`;
-    fetch(url, {
+  useEffect(() => {
+    if (id) {
+      const data = todo.filter(dt => dt._id == id)
+      const url = `http://localhost:8080/api/todo/${id}`;
+      fetch(url, {
         method: 'PUT',
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify(...data),
-    })
+      })
         .then(response => response.json())
         .then(result => {
-            if (result.modifiedCount) {
-               alert("status updated")
-            }else{
-                alert('nothing change')
-            }
+          if (result.modifiedCount) {
+            alert("status updated")
+          } else {
+            alert('nothing change')
+          }
         })
-  }
- 
+    }
 
-},[todo])
+
+  }, [todo])
 
   const onClick = (id) => {
     setId(id)
     setTodo(
-      todo.map((item) =>
-        {
-         return item._id === id ? { ...item, status: !item.status } : item
-        }
-      ) 
+      todo.map((item) => {
+        return item._id === id ? { ...item, status: !item.status } : item
+      }
+      )
     );
   };
+
+  const handleAllDelete = () => {
+    axios.delete('http://localhost:8080/api/todo')
+      .then(res => {
+        console.log('request here ', res.data.deletedCount);
+        if(res.data.deletedCount){
+          alert(res.data.deletedCount)
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
   return (
 
     <div className="p-5">
@@ -69,6 +82,7 @@ useEffect(() => {
         />
 
       </form>
+      <button className="w-25 btn btn-primary" onClick={handleAllDelete}>delete Completed Task</button>
       {
         text !== "" ? search.length ? search.map((todo, idx) => {
           const { _id, title, description, status } = todo;
@@ -77,7 +91,7 @@ useEffect(() => {
             {/* <p>DESCRIPTION : {description}</p> */}
             <p>STATUS : <input type="checkbox" defaultChecked={status} onClick={() => {
               onClick(_id)
-            }} /> {!status?"active":"completed"}</p>
+            }} /> {!status ? "active" : "completed"}</p>
 
           </div>
         }) : <div>
@@ -85,11 +99,11 @@ useEffect(() => {
         </div> : todo.length && todo.map((todoo, idx) => {
           const { _id, title, description, status } = todoo;
           return <div className="p-2 d-flex gap-5" key={idx}>
-            <Link  className="Link" to={`/details/${_id}`}><p>TASKS : {title}</p></Link>
+            <Link className="Link" to={`/details/${_id}`}><p>TASKS : {title}</p></Link>
             {/* <p>DESCRIPTION : {description}</p> */}
             <p>STATUS : <input type="checkbox" defaultChecked={status} onClick={() => {
               onClick(_id)
-            }} /> {!status?"active":"completed"}</p>
+            }} /> {!status ? "active" : "completed"}</p>
 
           </div>
         })
