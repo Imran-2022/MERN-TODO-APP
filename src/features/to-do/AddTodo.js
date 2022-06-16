9fefb0d5c4b3812c73dd96e0afa6b499a297ff0b
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-06-16 11:52:34 +0600

    form validation

--- src/features/to-do/AddTodo.js
+++ src/features/to-do/AddTodo.js
@@ -7,7 +7,8 @@ const AddTodo = () => {
     const navigate = useNavigate();
 
     const onSubmit = (data) => {
-        data.status=false;
+        data.status = false;
+
         axios.post('http://localhost:8080/api/todo', data)
             .then(res => {
                 if (res.data) {
@@ -16,13 +17,16 @@ const AddTodo = () => {
                     navigate("/show-todo", { replace: true });
                 }
             })
+
     }
-    
+
     return (
-        <div  className="m-5 p-5">
+        <div className="m-5 p-5">
             <form className="d-flex form-add flex-column gap-1 w-50" onSubmit={handleSubmit(onSubmit)}>
-                <input className='w-100' {...register("title", { required: true, maxLength: 20 })} placeholder="title"  autoFocus={true}/>
-                <textarea rows="4" cols="50"  {...register("description")} placeholder="description" />
+                <input className='w-100' {...register("title", { required: true, maxLength: 50 })} placeholder="title" autoFocus={true} />
+                {errors.title && <p>must have  a title with max length of 50 chars</p>}
+                <textarea rows="4" cols="50"  {...register("description", { required: true, maxLength: 256})} placeholder="description" />
+                {errors.description && <p>description has a max length of 256 chars</p>}
                 <input className='w-100 btn btn-primary' type="submit" value="ADD TODO" />
             </form>
         </div>
