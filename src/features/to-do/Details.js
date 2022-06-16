dfa22dca19917517932d5bb309730a529840df45
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-06-16 15:31:30 +0600

    complete task

--- src/features/to-do/Details.js
+++ src/features/to-do/Details.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-
+import { Button,Spinner } from 'react-bootstrap';
 const Details = () => {
     const { abc } = useParams()
     const navigate = useNavigate();
@@ -29,21 +29,41 @@ const Details = () => {
         }
     }
     return (
-        <div className="m-5 p-5">
-            <p>TITLE : {title}</p>
-            <p>DESCRIPTION : {description}</p>
-            <p>id : {_id}</p>
-            <button className="btn btn-primary me-4"
-                onClick={() => {
-                    handleDeleteBook(_id);
-                }}
-            >
-                Delete
-            </button>
-            <Link to="/edit-todo" state={{ _id, title, description, status }}>
-                <button className="btn btn-primary">Edit</button>
-            </Link>
-        </div>
+        <>
+            {
+                todo._id ? <div className="m-5 p-5 loadingbtn">
+                    <p>TITLE : {title}</p>
+                    <p>DESCRIPTION : {description}</p>
+                    {/* <p>id : {_id}</p> */}
+                    <div>
+                    <button className="btn btn-primary me-4"
+                        onClick={() => {
+                            handleDeleteBook(_id);
+                        }}
+                    >
+                        Delete
+                    </button>
+                    <Link to="/edit-todo" state={{ _id, title, description, status }}>
+                        <button className="btn btn-primary">Edit</button>
+                    </Link>
+                    </div>
+
+                </div> : <div className="loadingbtn">
+                    <Button variant="primary" className="w-25" disabled>
+                        <Spinner
+                            as="span"
+                            animation="grow"
+                            size="sm"
+                            role="status"
+                            aria-hidden="true"
+                        />
+                        Loading...
+                    </Button>
+                </div>
+            }
+
+        </>
+
 
     );
 };
